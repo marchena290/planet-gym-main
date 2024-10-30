06d57a6324d01d097e0e1968b898cef63abde37d
marchena290 <marchena452@gmail.com>
2024-10-29 23:58:38 -0600

    correcion de error

--- src/alpine.js
+++ src/alpine.js
@@ -1,8 +1,4 @@
 import Alpine from '../node_modules/alpinejs/dist/module.esm.js';
-import collapse from '../node_modules/@alpinejs/collapse/dist/module.esm.js';
-
-Alpine.plugin(collapse);
 
 window.Alpine = Alpine;
-
 Alpine.start();
--- src/app.js
+++ src/app.js
@@ -5,4 +5,39 @@ document.addEventListener('DOMContentLoaded', function(){
     mobileMenuButton.addEventListener('click', function(){
         mobileMenu.classList.toggle('hidden')
     });
+
+
+    // Funcion para manejar la visibilidad de los parrafos
+    function toggleVisibility() {
+        const parrafosAdiccionales = document.getElementById('parrafos-adicionales');
+        const toggleButton = document.getElementById('toggleButton');
+
+        if (parrafosAdiccionales.style.display === 'block') {
+            parrafosAdiccionales.style.display = 'none'
+            toggleButton.textContent = 'Ver más'
+        } else {
+            parrafosAdiccionales.style.display = 'block';
+            toggleButton.textContent = 'Ver menos'
+        }
+    }
+
+    document.getElementById('toggleButton').addEventListener('click', toggleVisibility);
+
+    // Asegurar que los parrafos esten visibles en pantallas grandes
+    window.addEventListener('resize',() => {
+        const parrafosAdiccionales = document.getElementById('parrafos-adicionales');
+        const toggleButton = document.getElementById('toggleButton');
+
+        if (window.innerWidth >= 768) {
+            parrafosAdiccionales.style.display = 'block';
+        } else {
+           parrafosAdiccionales.style.display = 'none';
+           toggleButton.style.display = 'Ver más' 
+        }
+
+
+    });
+
+    window.dispatchEvent(new Event('resize'));
+    
 });
